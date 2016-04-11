c237aea063ff9cb7919875c7ab58e05001122d86
Sebastian Vidrio <svidrio@gmail.com>
2016-04-11 13:53:40 -0700

    STEAM-139: Adding basic kerberos support

--- lib/yarn/yarn.go
+++ lib/yarn/yarn.go
@@ -1,7 +1,9 @@
 package yarn
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -9,10 +11,49 @@ import (
 	"strconv"
 )
 
+// If there is a open ticket, does nothing, else kinits a new session
+func kCheck() {
+	if err := exec.Command("klist", "-s").Run(); err != nil { // returns no err if there's an open ticket
+		log.Println("Kerberos login required")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("User: ")
+		user, _ := reader.ReadString('\n')
+		user = user[:len(user)-1]
+
+		kInit(user)
+	}
+}
+
+func kInit(user string) {
+	cmd := exec.Command("kinit", user)
+	// stdout, err := cmd.StdoutPipe()
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatalln("InPipe")
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalln("Could not execute")
+	}
+
+	go io.Copy(stdin, os.Stdin)
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalln("Failed to authenticate.")
+	}
+}
+
 // StartCloud starts a yarn cloud by shelling out to hadoop
 //
 // This process needs to store the job-ID to kill the process in the future
 func StartCloud(size int) {
+
+	kCheck()
 
 	cmdArgs := []string{
 		"jar",                //
@@ -51,6 +92,10 @@ func StartCloud(size int) {
 
 // StopCloud kills a hadoop cloud by shelling out a command based on the job-ID
 func StopCloud(id string) {
+
+	kCheck()
+
+	log.Println("Attempting to stop cloud...")
 	cmdStop := exec.Command("hadoop", "job", "-kill", "job_"+id)
 	if out, err := cmdStop.CombinedOutput(); err != nil {
 		log.Println("Failed to shutdown hadoop.")
