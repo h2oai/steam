78503ea2fa2a25cd1a052e30893d76bcf7fcde06
Magnus Stensmo <mstensmo@gmail.com>
2016-04-28 19:28:49 -0700

    better index.html for scoring service

--- scoring-service-builder/makewar-extra/predict.js
+++ scoring-service-builder/makewar-extra/predict.js
@@ -1,5 +1,5 @@
 // predict.js
-// H2O Prediction Service
+// H2O Scoring Service
 
 // Sort values if not sorted properly
 // This is needed because c-1, c-2, ... are sorted alphabetically which is wrong
@@ -30,22 +30,19 @@ function sortValues(values) {
 function showModel(model, element) {
     var names = model["m"]["_names"];
     var domains = model["m"]["_domains"];
-//    console.log("names = " + JSON.stringify(names));
     var domainMap = model["domainMap"];
 
     form = '<form id="allparams" action="" method="get">';
     for(i in names) {
         n = names[i];
-        i1 = i + 1;
+        i1 = Number(i) + 1;
         form += '<p><b>' + i1 + '. ' + n + '</b> ';
         domain = domains[i];
         domain = sortValues(domain);
         card = domain == null ? 0 : domain.length;
-        //console.log((i + 1) + "  " + card);
-//        console.log(domain, card);
-        //console.log("cardinality " + card);
+
         if (card < 2)
-          form += '<input type="text" name="' + n + '">';
+          form += '<input type="text" size=40 name="' + n + '">';
         else if (card <= 12) {
           for (i = 0; i < card; i += 1) {
             form += '<input type="radio" name="' + n + '" value="' + domain[i] + '"> ' + domain[i] + '</input>\n';
@@ -63,71 +60,73 @@ function showModel(model, element) {
     }
     form += '<input type="button" name="okbutton" value="PREDICT" onClick="runpred2(this.form)">';
 
-//    form += '<input type="button" name="okbutton" value="PREDICT" onClick="runpred(this.form)"><p>' +
     form += ' <input type="reset" value="CLEAR">';
 
     form += '</form>';
-//    console.log(form);
     element.innerHTML += form;
 
-    // Alternative query string form
     form += '<p>';
 
 }
 
-function showInfo() {
-$.get('/info', function(data, status) {
+function showInputParameters() {
+    $.get('/info', function(data, status) {
     // show result
     var info = document.querySelector(".input");
     showModel(data, info);
   },'json');
 }
 
+function showResult(div, data) {
+    label = data["label"];
+    index = data["labelIndex"];
+    probs = data["classProbabilities"];
+    prob = probs[index];
+
+    result = "Label: <b>" + label + "</b><br>" +
+      "Probability: <b>" + (prob * 100.0).toFixed(1) + "%</b><p>" +
+      "Class Probabilities: [" + probs + "]<br>" +
+      "Label Index: " + index;
+    div.innerHTML = result;
+}
+
+function showUrl(pardiv, params) {
+  url = window.location.href + "predict?" + params;
+  // remove empty parameters returned by serialize.
+  params.replace(/[\w]+=&/g, "").replace(/&?[\w]+=$/g, "");
+  pardiv.innerHTML = '<a href="' + url + '" target="_blank"><code>' + url + '</code>';
+}
 
 function runpred(form) {
   params = form.p.value;
-  console.log(params);
   pardiv = document.querySelector(".params");
-
   // add link that opens in new window
-  url = window.location.href + "predict?" + params;
-  pardiv.innerHTML = '<a href="' + url + '" target="_blank"><code>' + url + '</code>';
+  showUrl(pardiv, params);
 
   cmd = '/predict?' + params;
+  div = document.querySelector(".results");
   res = $.get(cmd, function(data, status) {
-     var div = document.querySelector(".results");
-     div.innerHTML = status + "   " + JSON.stringify(data);
+    showResult(div, data);
   },'json');
 
 }
 
 function runpred2(form) {
   var params = $('#allparams').serialize();
-  // remove empty parameters returned by serialize.
-  params = params.replace(/[\w]+=&/g, "").replace(/&?[\w]+=$/g, "");
-  console.log(params);
   pardiv = document.querySelector(".params");
-
   // add link that opens in new window
-  url = window.location.href + "predict?" + params;
-  pardiv.innerHTML = '<a href="' + url + '" target="_blank"><code>' + url + '</code>';
+  showUrl(pardiv, params);
 
   cmd = '/predict?' + params;
+  div = document.querySelector(".results");
   res = $.get(cmd, function(data, status) {
-     div = document.querySelector(".results");
-     label = data["label"];
-     index = Number(data["index"]);
-     probs = data["classProbabilities"];
-     prob = probs[index];
-     console.log(probs);
-     console.log(prob);
-     div.innerHTML = status + "   " + JSON.stringify(data);
-  },'json');
+    showResult(div, data);
+   },'json');
 
 }
 
 // main
-showInfo();
+showInputParameters();
 
 
 
