0f32dbbc8bb298f17868061edb34b928b477a894
Sebastian Vidrio <svidrio@gmail.com>
2016-06-24 18:20:06 -0700

    STEAM-151: no username/password should error out

--- master/az.go
+++ master/az.go
@@ -1,11 +1,13 @@
 package master
 
 import (
-	"github.com/abbot/go-http-auth"
-	"github.com/h2oai/steamY/master/az"
-	"github.com/h2oai/steamY/master/data"
+	"fmt"
 	"log"
 	"net/http"
+
+	auth "github.com/abbot/go-http-auth"
+	"github.com/h2oai/steamY/master/az"
+	"github.com/h2oai/steamY/master/data"
 )
 
 type DefaultAz struct {
@@ -22,6 +24,11 @@ func (a *DefaultAz) Authenticate(username string) string {
 		log.Printf("User %s read failed: %s\n", username, err)
 		return ""
 	}
+
+	if pz == nil {
+		log.Printf("User %s does not exist\n", username)
+		return ""
+	}
 	log.Println("User logged in:", username)
 	return pz.Password()
 }
@@ -29,5 +36,14 @@ func (a *DefaultAz) Authenticate(username string) string {
 func (a *DefaultAz) Identify(r *http.Request) (az.Principal, error) {
 	username := r.Header.Get(auth.AuthUsernameHeader)
 	log.Println("User identified:", username)
-	return a.ds.NewPrincipal(username)
+	pz, err := a.ds.NewPrincipal(username)
+	if err != nil {
+		return nil, err
+	}
+
+	if pz == nil {
+		return nil, fmt.Errorf("User %s does not exist\n", username)
+	}
+
+	return pz, nil
 }
