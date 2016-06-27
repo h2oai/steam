d051c34c052695cab06b6bc7276f5ad8c8df5585
Prithvi Prabhu <prithvi@0xdata.com>
2016-06-27 11:28:45 -0700

    STEAM-196 Break out "serve" command into separate files

--- cli/main.go
+++ cli/main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/h2oai/steamY/master"
 	"github.com/spf13/cobra"
 )
 
@@ -147,88 +146,4 @@ func parseHelp(text string) (*Doc, error) {
 		d[1],
 		d[1] + "\n\n" + d[2],
 	}, nil
-}
-
-//
-// Commands
-//
-
-var serveHelp = `
-serve [agent-type]
-Lauch a new service.
-Examples:
-
-    $ steam serve master
-`
-
-func serve(c *context) *cobra.Command {
-	cmd := newCmd(c, serveHelp, nil)
-	cmd.AddCommand(serveMaster(c))
-	return cmd
-}
-
-var serveMasterHelp = `
-master
-Launch the Steam master.
-Examples:
-
-    $ steam serve master
-`
-
-func serveMaster(c *context) *cobra.Command {
-	var (
-		webAddress                string
-		workingDirectory          string
-		clusterProxyAddress       string
-		compilationServiceAddress string
-		scoringServiceHost        string
-		enableProfiler            bool
-		yarnEnableKerberos        bool
-		yarnUserName              string
-		yarnKeytab                string
-		dbName                    string
-		dbUserName                string
-		dbSSLMode                 string
-		superuserName             string
-		superuserPassword         string
-	)
-
-	opts := master.DefaultOpts
-
-	cmd := newCmd(c, serveMasterHelp, func(c *context, args []string) {
-		master.Run(c.version, c.buildDate, &master.Opts{
-			webAddress,
-			workingDirectory,
-			clusterProxyAddress,
-			compilationServiceAddress,
-			scoringServiceHost,
-			enableProfiler,
-			yarnEnableKerberos,
-			yarnUserName,
-			yarnKeytab,
-			dbName,
-			dbUserName,
-			dbSSLMode,
-			superuserName,
-			superuserPassword,
-		})
-	})
-
-	cmd.Flags().StringVar(&webAddress, "web-address", opts.WebAddress, "Web server address (\"<ip>:<port>\" or \":<port>\").")
-	cmd.Flags().StringVar(&workingDirectory, "working-directory", opts.WorkingDirectory, "Working directory for application files.")
-	cmd.Flags().StringVar(&clusterProxyAddress, "cluster-proxy-address", opts.ClusterProxyAddress, "Cluster proxy address (\"<ip>:<port>\" or \":<port>\")")
-	cmd.Flags().StringVar(&compilationServiceAddress, "compilation-service-address", opts.CompilationServiceAddress, "Model compilation service address (\"<ip>:<port>\")")
-	cmd.Flags().StringVar(&scoringServiceHost, "scoring-service-address", opts.ScoringServiceHost, "Address to start scoring services on (\"<ip>\")")
-	cmd.Flags().BoolVar(&enableProfiler, "profile", opts.EnableProfiler, "Enable Go profiler")
-	cmd.Flags().BoolVar(&yarnEnableKerberos, "yarn-enable-kerberos", opts.YarnKerberosEnabled, "Enable Kerberos authentication. Requires username and keytab.") // FIXME: Kerberos authentication is being passed by admin to all
-	cmd.Flags().StringVar(&yarnUserName, "yarn-username", opts.YarnUserName, "Username to enable Kerberos")
-	cmd.Flags().StringVar(&yarnKeytab, "yarn-keytab", opts.YarnKeytab, "Keytab file to be used with Kerberos authentication")
-	cmd.Flags().StringVar(&dbName, "db-name", opts.DBName, "Database name to use for application data storage")
-	cmd.Flags().StringVar(&dbUserName, "db-username", opts.DBUserName, "Database username to connect as")
-	cmd.Flags().StringVar(&dbSSLMode, "db-ssl-mode", opts.DBSSLMode, "Database connection SSL mode: one of 'disable', 'require', 'verify-ca', 'verify-full'")
-	cmd.Flags().StringVar(&superuserName, "superuser-name", opts.SuperuserName, "Set superuser username (required for first-time-use only)")
-	cmd.Flags().StringVar(&superuserPassword, "superuser-password", opts.SuperuserPassword, "Set superuser password (required for first-time-use only)")
-
-	return cmd
-
 }
