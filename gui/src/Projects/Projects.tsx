d6f87cc792e930e8bbebb35af808d5f72affe528
Justin Loyola <justin@h2o.ai>
2016-07-10 20:53:59 -0700

    Add splash screen

--- gui/src/Projects/Projects.tsx
+++ gui/src/Projects/Projects.tsx
@@ -3,8 +3,7 @@
  */
 
 import * as React from 'react';
-import * as _ from 'lodash';
-import NewProject from './components/NewProject';
+import WelcomeSplashScreen from './components/WelcomeSplashScreen';
 
 interface Props {
   leaderboard
@@ -14,11 +13,12 @@ interface DispatchProps {
   fetchLeaderboard: Function
 }
 
+        // <NewProject></NewProject>
 export default class Projects extends React.Component<Props & DispatchProps, any> {
   render(): React.ReactElement<HTMLDivElement> {
     return (
       <div className="projects">
-        <NewProject></NewProject>
+        <WelcomeSplashScreen/>
       </div>
     );
   }
