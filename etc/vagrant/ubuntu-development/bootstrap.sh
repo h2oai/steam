fcfa0820623dc12871a617d6dc71962753642c08
Prithvi Prabhu <prithvi@0xdata.com>
2016-07-28 15:53:59 -0700

    Vagrant development setup on Ubuntu 14.04 LTS.

---
+++ etc/vagrant/ubuntu-development/bootstrap.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+#
+# ubuntu/trusty64 provisioning for Steam
+# 
+
+# Update
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Java PPA
+sudo add-apt-repository -y ppa:webupd8team/java
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+
+# Update
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Git
+sudo apt-get install -y git
+
+# Java
+sudo apt-get install -y oracle-java7-installer
+
+# Node.js
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Golang
+wget https://storage.googleapis.com/golang/go1.6.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.6.3.linux-amd64.tar.gz
+rm go1.6.3.linux-amd64.tar.gz
+
+# Set up Golang env
+echo "export PATH=\$PATH:/usr/local/go/bin" >> /home/vagrant/.profile
+mkdir -p /home/vagrant/go
+echo "export GOPATH=/home/vagrant/go" >> /home/vagrant/.profile
+
+echo "**************************************************"
+echo "* VM ready. Complete these steps manually:       *"
+echo "* -----------------------------------------------*"
+echo "* vagrant ssh                                    *"
+echo "* cd ~/go                                        *"
+echo "* git clone https://github.com/h2oai/steamY.git  *"
+echo "* make                                           *"
+echo "**************************************************"
