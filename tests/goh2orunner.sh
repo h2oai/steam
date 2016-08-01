c5bca159577903613ee41f05e5bada677eec705c
Sebastian Vidrio <svidrio@gmail.com>
2016-08-01 14:34:05 -0700

    STEAM-280: exit with non zero value if go test fails

--- tests/goh2orunner.sh
+++ tests/goh2orunner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start H2O and create a start log + store pid
-java -jar ../../var/temp/h2o-$H2OVERSION/h2o.jar > start.log 2>&1 &
+java -jar ../var/temp/h2o-$H2OVERSION/h2o.jar > start.log 2>&1 &
 PID=$!
 echo "Started h2o with pid ${PID}"
 
@@ -17,7 +17,9 @@ echo "Preparing models in cluster"
 python h2o-setup.py $ADDRESS
 
 # Run GO tests here
-go test ../../master/data -v -coverprofile=masterdata.cov
-go test ../../master/web -v -coverprofile=masterweb.cov
+go test ../master/web -v -coverprofile=masterweb.cov
+t1=$?
 
 kill -9 $PID
+
+exit $t1
