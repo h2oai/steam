4420b5e214dac30bc3c33adafb9f7e66be3a25e1
Prithvi Prabhu <prithvi@0xdata.com>
2016-08-05 16:40:51 -0700

    STEAM-414 Fix typo

--- master/web/setup_test.go
+++ master/web/setup_test.go
@@ -57,7 +57,7 @@ func init() {
 
 func newTest(t *testing.T) *test {
 	dbOpts := driverDBOpts{
-		data.Connection {DbName:"steam", User:"steam", SSLMode:"disable"}
+		data.Connection{DbName: "steam", User: "steam", SSLMode: "disable"},
 		superuser,
 		superuser,
 	}
