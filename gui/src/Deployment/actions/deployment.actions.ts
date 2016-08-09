f1354af889b39d96b0c9db6e3bfd066d04cad6da
Justin Loyola <justin@h2o.ai>
2016-08-09 14:14:44 -0700

    Set attributes for files

--- gui/src/Deployment/actions/deployment.actions.ts
+++ gui/src/Deployment/actions/deployment.actions.ts
@@ -32,11 +32,15 @@ export function uploadPackage(projectId: number, packageName: string, form) {
   return (dispatch) => {
     dispatch(uploadingPackage());
     let formFiles: NodeListOf<HTMLInputElement> = form.querySelectorAll('input[type="file"');
-    for (let i = 0; i < formFiles.length; i++) {
-      let data = new FormData();
-      for (let j = 0; j < formFiles[i].files.length; j++) {
-        data.append('file', formFiles[i].files[j]);
-        Remote.createPackage(projectId, packageName, (error) => {
+    Remote.createPackage(projectId, packageName, (error) => {
+      for (let i = 0; i < formFiles.length; i++) {
+        let data = new FormData();
+        for (let j = 0; j < formFiles[i].files.length; j++) {
+          let isMain = false;
+          if (formFiles[i].name === 'selectMain') {
+            isMain = true;
+          }
+          data.append('file', formFiles[i].files[j]);
           if (error) {
             dispatch(openNotification('error', error.toString(), null));
             return;
@@ -46,12 +50,24 @@ export function uploadPackage(projectId: number, packageName: string, form) {
             method: 'post',
             body: data
           }).then(() => {
-            dispatch(finishUploadingPackage());
-            dispatch(fetchPackages(projectId));
+            console.log(formFiles[i].files[j].name, i, j);
+            if (isMain) {
+              Remote.setAttributesForPackage(projectId, packageName, JSON.stringify({main: formFiles[i].files[j].name}), (error) => {
+                if (error) {
+                  dispatch(openNotification('error', error, null));
+                  return;
+                }
+                dispatch(finishUploadingPackage());
+                dispatch(fetchPackages(projectId));
+              });
+            } else {
+              dispatch(finishUploadingPackage());
+              dispatch(fetchPackages(projectId));
+            }
           });
-        });
+        }
       }
-    }
+    });
   };
 }
 
