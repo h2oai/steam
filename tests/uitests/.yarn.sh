5861e771eaaf9ad52abe0aabc37a8e64b9449d34
Patrick Rice <patrick@h2o.ai>
2016-09-21 11:45:44 -0700

    automated steam launching on yarn clusters

---
+++ tests/uitests/.yarn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/patrick
+
+rm -f steamY-develop-linux-amd64.tar.gz
+rm -rf steam-develop-linux-amd64
+
+curl -O https://s3.amazonaws.com/steam-release/steamY-develop-linux-amd64.tar.gz
+tar xvf steamY-develop-linux-amd64.tar.gz
+cd steam-develop-linux-amd64
+./steam serve master --superuser-name patrick --superuser-password superuser 
+
--- tests/uitests/run-kerberos.sh
+++ tests/uitests/run-kerberos.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-su -c ./.kerberos.sh
+scp .kerberos.sh patrick@mr-0xc7:~/
+ssh root@mr-0xc7 /home/patrick/.kerberos.sh
+ssh root@mr-0xc7 killall steam
+
 
