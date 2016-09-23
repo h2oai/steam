8317f623d2d26d9a6b5d8d311b40256333af0d13
Justin Loyola <justin@h2o.ai>
2016-09-22 17:59:13 -0700

    Change license text

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
+/*
+Copyright (C) 2016 H2O.ai, Inc. <http://h2o.ai/>
+Everyone is permitted to copy and distribute verbatim copies
+of this license document, but changing it is not allowed.
+ */
+
 package main
 
-/*
-		    GNU AFFERO GENERAL PUBLIC LICENSE
-                       Version 3, 19 November 2007
-
- Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
- Everyone is permitted to copy and distribute verbatim copies
- of this license document, but changing it is not allowed.
- */
 
 import (
 	cli "github.com/h2oai/steamY/cli2"
