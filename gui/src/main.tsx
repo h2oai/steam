2eb14e5ed6b27155767eab8c6e3a2a9bc15e4512
Justin Loyola <justin@h2o.ai>
2016-09-22 17:49:53 -0700

    Add license to index and main.tsx

--- gui/src/main.tsx
+++ gui/src/main.tsx
@@ -1,5 +1,10 @@
 /**
- * Created by justin on 6/17/16.
+ GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3, 19 November 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
  */
 
 import * as React from 'react';
