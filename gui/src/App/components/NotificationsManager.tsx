f89150f418ccfa86dfb873a996a881653a1c03f0
Ravi Purushotma <rpurushotma@gmail.com>
2016-09-27 13:07:37 -0700

    license header

--- gui/src/App/components/NotificationsManager.tsx
+++ gui/src/App/components/NotificationsManager.tsx
@@ -1,3 +1,20 @@
+/*
+ Copyright (C) 2016 H2O.ai, Inc. <http://h2o.ai/>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import * as React from 'react';
 import * as _ from 'lodash';
 import { connect } from 'react-redux';
@@ -79,7 +96,7 @@ export class NotificationsManager extends React.Component<Props & DispatchProps,
   startTimeoutTimer() {
     this.timeoutTimerID = setTimeout(this.onTimeout.bind(this), this.TIMEOUT_LENGTH);
   }
-  
+
   onTimeout() {
     for (let notificationData of this.props.notifications.allNotifications) {
       notificationData.isActive = false;
