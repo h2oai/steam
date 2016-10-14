a968a8b83abfffededce7a7d379007828e84a2f4
Justin Loyola <justin@h2o.ai>
2016-10-14 10:01:51 -0700

    Adds image classification to prediction service UI

--- prediction-service-builder/src/main/webapp/extra/predict.js
+++ prediction-service-builder/src/main/webapp/extra/predict.js
@@ -1,19 +1,19 @@
 /*
-Copyright (C) 2016 H2O.ai, Inc. <http://h2o.ai/>
+ Copyright (C) 2016 H2O.ai, Inc. <http://h2o.ai/>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // TODO
 // check for timeouts in $.get calls
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (function(window) {
   'use strict';
   var outputDomain;
+  var API_HOST = '';
+  var isBinaryPrediction = false;
 
 
 
@@ -70,20 +72,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       form += '<div class="col-sm-7 col-md-7 col-lg-8">'
 
       if (card < 2) {
-        form += '<input class="form-control" type="text" name="' + n + '" oninput="updateUrl(event);">';
+        if (isBinaryPrediction && i1 === 1) {
+          form += '<input type="file" name="' + n + '" onchange="readURL(this);" style="display: block">';
+          form += '<img id="image-preview"/>';
+        } else {
+          form += '<input class="form-control" type="text" name="' + n + '" oninput="updateUrl(event);">';
+        }
       } else if (card <= 2) {
         for (var i = 0; i < card; i += 1) {
           form += '<input type="radio" name="' + n + '" value="' + domain[i] + '" onclick="updateUrl(event);"> ' + domain[i] + '</input>\n';
         }
       } else {
-        form += '<select name="' + n + '" class="form-control" onchange="updateUrl(event);">';
-        form += '<option value=""></option>';
-        for (var v of domain) {
-          form += '<option value="' + v + '">' + v + '</option>';
+        if (!isBinaryPrediction) {
+          form += '<select name="' + n + '" class="form-control" onchange="updateUrl(event);">';
+          form += '<option value=""></option>';
+          for (var v of domain) {
+            form += '<option value="' + v + '">' + v + '</option>';
+          }
+          form += '</select>';
         }
-        form += '</select>';
       }
       form += '</div></div>\n';
+      if (isBinaryPrediction) {
+        break;
+      }
     }
 
     outputDomain = domains[i1];
@@ -110,8 +122,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       $('#query-link').css({
         cursor: 'pointer'
       })
-      .unbind('click')
-      .attr('href', 'predict?' + queryString);
+        .unbind('click')
+        .attr('href', 'predict?' + queryString);
     }
   };
 
@@ -124,15 +136,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     $('#query-link').css({
       cursor: 'not-allowed'
     })
-    .bind('click', function(e) {
-      e.preventDefault();
-    })
-    .attr('href', '');
+      .bind('click', function(e) {
+        e.preventDefault();
+      })
+      .attr('href', '');
   };
 
   function showInputParameters() {
-    $.get('/info', function(data, status) {
+    $.get(API_HOST + '/info', function(data, status) {
       // show result
+      console.log(data);
+      if (data.m._problem_type === 'image_classification') {
+        isBinaryPrediction = true;
+      }
       var info = document.querySelector("#fs-params");
       showModel(data, info);
     }, 'json');
@@ -142,18 +158,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     var result = '<legend>Model Predictions</legend>'
 
-    if ("classProbabilities" in data) {
+    if (data.classProbabilities) {
 
 
       // binomial and multinomial
-      var label = data["label"];
-      var index = data["labelIndex"];
-      var probs = data["classProbabilities"];
+      var label = data.label;
+      var index = data.labelIndex;
+      var probs = data.classProbabilities;
       var prob = probs[index];
 
       result += '<p>Predicting <span class="labelHighlight">' + label + '</span>';
       if (probs.length == 2) {
-          result += ' based on max F1 threshold </p>';
+        result += ' based on max F1 threshold </p>';
       }
       result += ' </p>';
       result += '<table class="table" id="modelPredictions"> \
@@ -167,13 +183,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                    <tbody> \
                   ';
 
-      for (var label_i in outputDomain) {
-        if (parseInt(label_i) === index ){
-          result += '<tr class="rowHighlight">'
-        } else {
-          result += '<tr>'
+      if (isBinaryPrediction) {
+        var labelProbabilitiesMapping = [];
+        outputDomain.map(function(label, i) {
+          var labelProbMap = {};
+          labelProbMap.label = outputDomain[i];
+          labelProbMap.probability = probs[i];
+          if (i === index) {
+            labelProbMap.predicted = true;
+          }
+          labelProbMap.originalIndex = i;
+          labelProbabilitiesMapping.push(labelProbMap);
+        });
+        labelProbabilitiesMapping.sort(function(a, b) {
+          return b.probability - a.probability;
+        });
+        for (var i = 0; i < labelProbabilitiesMapping.length; i++) {
+          if (labelProbabilitiesMapping[i].predict === true) {
+            result += '<tr class="rowHighlight">'
+          } else {
+            result += '<tr>'
+          }
+          result += '<td>' + labelProbabilitiesMapping[i].originalIndex + '</td><td>' + labelProbabilitiesMapping[i].label + '</td> <td>' + labelProbabilitiesMapping[i].probability.toFixed(4) + '</td></tr>';
         }
-        result += '<td>' + label_i + '</td><td>' + outputDomain[label_i] + '</td> <td>' + probs[label_i].toFixed(4) + '</td></tr>';
+      } else {
+        for (var label_i in outputDomain) {
+          if (parseInt(label_i) === index ){
+            result += '<tr class="rowHighlight">'
+          } else {
+            result += '<tr>'
+          }
+          result += '<td>' + label_i + '</td><td>' + outputDomain[label_i] + '</td> <td>' + probs[label_i].toFixed(4) + '</td></tr>';
+        }
       }
 
       result += '</tbody></table>';
@@ -195,7 +236,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     }
 
     div.innerHTML = result;
-}
+  }
 
   function showUrl(pardiv, params) {
     // remove empty parameters returned by serialize.
@@ -213,14 +254,43 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
   function predResults(params) {
     var div = document.querySelector("#modelPredictions");
-    var cmd = '/predict?' + params;
-    $.get(cmd, function(data, status) {
-      showResult(div, status, data);
-    }, 'json')
-      .fail(function(data, status, error) {
-        var down = "<b>GET to " + cmd + " Failed</b> " + error;
-        div.innerHTML = down + "<br><b>status</b> " + status;
+    var path = '/predict?';
+    if (isBinaryPrediction) {
+      path = '/predictbinary';
+    }
+    var cmd = API_HOST + path + params;
+    console.log(cmd);
+    if (isBinaryPrediction) {
+      var form = $('#allparams');
+      var data = new FormData();
+      $.each($(form).find('input[type="file"]')[0].files, function(i, file) {
+        data.append('binary_C' + (i + 1), file);
       });
+      $.ajax({
+        url: API_HOST + path,
+        data: data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        type: 'POST',
+        success: function(data, status){
+          console.log(data, status);
+          showResult(div, status, JSON.parse(data));
+        }
+      })
+        .fail(function(data, status, error) {
+          var down = "<b>GET to " + cmd + " Failed</b> " + error;
+          div.innerHTML = down + "<br><b>status</b> " + status;
+        });
+    } else {
+      $.get(cmd, function(data, status) {
+        showResult(div, status, data);
+      }, 'json')
+        .fail(function(data, status, error) {
+          var down = "<b>GET to " + cmd + " Failed</b> " + error;
+          div.innerHTML = down + "<br><b>status</b> " + status;
+        });
+    }
   }
 
   window.runpred2 = function(form) {
@@ -240,7 +310,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
   function predResultsPost(params) {
     var pardiv = document.querySelector(".curl");
-    var cmd = '/pypredict';
+    var cmd = API_HOST + '/pypredict';
     showCurl(pardiv, params, cmd);
 
     var div = document.querySelector(".results");
@@ -248,8 +318,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       showResult(div, status, data);
     }, 'json')
       .fail(function(data, status, error) {
-         var down = "<b>POST to " + cmd + " Failed</b> " + error;
-         div.innerHTML = down + "<br><b>status</b> " + status;
+        var down = "<b>POST to " + cmd + " Failed</b> " + error;
+        div.innerHTML = down + "<br><b>status</b> " + status;
       });
 
   }
@@ -261,7 +331,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
   function predResultsPostJar(params) {
     var pardiv = document.querySelector(".curl");
-    var cmd = '/predict';
+    var cmd = API_HOST + '/predict';
     showCurl(pardiv, params, cmd);
 
     var div = document.querySelector(".results");
@@ -273,8 +343,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       showResult(div, status, data);
     }, 'json')
       .fail(function(data, status, error) {
-         var down = "<b>POST to " + cmd + " Failed</b> " + error;
-         div.innerHTML = down + "<br><b>status</b> " + status;
+        var down = "<b>POST to " + cmd + " Failed</b> " + error;
+        div.innerHTML = down + "<br><b>status</b> " + status;
       });
 
   }
@@ -357,7 +427,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
   function showStatistics() {
-    var cmd = '/stats';
+    var cmd = API_HOST + '/stats';
     var res = $.get(cmd, function(data, status) {
       var divs = document.querySelector("#modelStats");
       showStats(divs, data);
@@ -384,35 +454,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     showStatistics();
 
     $('#stats-btn').click(function() {
-      window.open('/stats', '_blank');
+      window.open(API_HOST + '/stats', '_blank');
     });
 
     $('.file-icon').click(function(){
-    $(this).closest('.input-inline-row').find("input[type='file']").trigger('click');
+      $(this).closest('.input-inline-row').find("input[type='file']").trigger('click');
     });
 
     $('.file-input').click(function(){
-        $(this).closest('.input-inline-row').find("input[type='file']").trigger('click');
+      $(this).closest('.input-inline-row').find("input[type='file']").trigger('click');
     });
 
     $('input[type="file"]').change(function(){
 
-        var names = [];
-        for (var i = 0; i < $(this).get(0).files.length; ++i) {
-            names.push( $(this).get(0).files[i].name );
-        }
+      var names = [];
+      for (var i = 0; i < $(this).get(0).files.length; ++i) {
+        names.push( $(this).get(0).files[i].name );
+      }
 
-        $(this).closest('.input-inline-row').find("input[type='text']").val(names);
+      $(this).closest('.input-inline-row').find("input[type='text']").val(names);
     });
 
     $('#batch-predict').click(function(){
-        if ( !$('#batch form input[name="inpfile"]').val() ){
-           alert('You must select a files with multiple lines of JSON predictions.');
-        }
+      if ( !$('#batch form input[name="inpfile"]').val() ){
+        alert('You must select a files with multiple lines of JSON predictions.');
+      }
 
-        $('#batch form input[name="inpfile"]').get(0).files = $('#batch form input[name="inpfile"]').get(0).files;
+      $('#batch form input[name="inpfile"]').get(0).files = $('#batch form input[name="inpfile"]').get(0).files;
 
-        $('#batch form').submit();
+      $('#batch form').submit();
     });
 
     $('#batch-reset').click(function(){
@@ -421,11 +491,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     });
 
     $('#predict-btn').click(function(){
-        $('#allparams').submit();
+      $('#allparams').submit();
     })
 
     $('#reset-btn').click(function(){
-        $('#allparams')[0].reset();       
+      $('#allparams')[0].reset();
     })
   });
+
+  window.readURL = function(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#image-preview')
+          .attr('src', e.target.result)
+          .css({
+            maxWidth: '100%'
+          });
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
 })(window);
