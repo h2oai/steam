a02cc8e2fc6cd042f4062dd7c54047ec33ebc189
justinloyola <justin@0xdata.com>
2016-10-19 18:07:35 -0700

    Revert "Revert "Deepwater""

---
+++ prediction-service-builder/examples/deepwater/test-example.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+curl -X POST \
+--form binary_image=@dog.jpg \
+--form abc=def \
+--form data='{Dest: SFO, Orig: JFK}' \
+http://localhost:55001/predictbinary
+
+echo "test1 bird"
+
+curl -X POST \
+--form binary_C1=@test1.jpg \
+http://localhost:55001/predictbinary
+
+echo "test2 dog"
+
+curl -X POST \
+--form binary_C1=@test2.jpg \
+http://localhost:55001/predictbinary
+
+echo "url of dog"
+
+#curl -X POST \
+#--form C1="https://c1.staticflickr.com/1/225/515776742_bce2e6dbea_d.jpg" \
+#http://localhost:55001/predictbinary
