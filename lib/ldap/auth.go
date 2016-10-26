16cc8e8d5a56f5305009b5493f8025aa0fab1c68
Sebastian Vidrio <Seb@Sebastians-iMac.local>
2016-10-26 14:42:50 -0700

    STEAM-496: Implement authentication using LDAP

--- lib/ldap/auth.go
+++ lib/ldap/auth.go
@@ -1,6 +1,12 @@
 package ldap
 
-import "github.com/abbot/go-http-auth"
+import (
+	"net/http"
+
+	"golang.org/x/net/context"
+
+	auth "github.com/abbot/go-http-auth"
+)
 
 type BasicLdapAuth struct {
 	Realm string
@@ -12,4 +18,18 @@ type BasicLdapAuth struct {
 	// Headers used by authenticator. Set to ProxyHeaders to use with
 	// proxy server. When nil, NormalHeaders are used.
 	Headers *auth.Headers
+}
+
+func (a *BasicLdapAuth) Wrap(auth.AuthenticatedHandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if username := "/* VERIFY VALID USER */"; username == "" {
+			/* RETURN BASIC AUTH REQUIRED HEADERS */
+		} else {
+			ar := *auth.AuthenticatedRequest{Request: *r, Username: username}
+			wrapped(a, ar)
+		}
+	}
+}
+
+func (a *BasicLdapAuth) NewContext(ctx context.Context, r *http.Request) context.Context {
 }
