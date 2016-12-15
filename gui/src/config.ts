8312a05f54570b7560c091e06b86b6d9be1b410f
Justin Loyola <justin@h2o.ai>
2016-12-15 10:05:41 -0800

    Add config

---
+++ gui/src/config.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by justin on 12/14/16.
+ */
+
+export class Config {
+  public static environment: 'development' | 'production' = 'development';
+  public static API_HOST: string = 'http://localhost:9000'
+}
