2771a88112f4efe9cd36738ee84dcaf858aee22b
Justin Loyola <justin@h2o.ai>
2017-01-11 18:17:24 -0800

    Clean up

--- gui/src/Users/actions/users.actions.ts
+++ gui/src/Users/actions/users.actions.ts
@@ -599,7 +599,6 @@ export function fetchPermissionsWithRoles() {
               flags
             });
           }
-          console.log(output);
           dispatch(receivePermissionsByRole(output));
         });
       });
