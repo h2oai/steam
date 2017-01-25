b7b437671b582788f8c56d9b6d558c98c6674540
Sebastian Vidrio <svidrio@gmail.com>
2017-01-24 19:40:58 -0800

    STEAM-711: Check for CN and DN during group search

--- lib/ldap/ldap_test.go
+++ lib/ldap/ldap_test.go
@@ -5,8 +5,18 @@ import (
 	"reflect"
 	"testing"
 
+	"os"
+
 	ldap "gopkg.in/ldap.v2"
 )
+
+var (
+	adPass string
+)
+
+func init() {
+	adPass = os.Getenv("AD_PASSWORD")
+}
 
 func TestFromDatabase(t *testing.T) {
 	type args struct {
@@ -63,95 +73,6 @@ func TestFromDatabase(t *testing.T) {
 	}
 }
 
-func TestLdap_checkGroup(t *testing.T) {
-	conn, err := ldap.Dial("tcp", "ldap.0xdata.loc:389")
-	if err != nil {
-		t.Skipf("no ldap connection")
-		return
-	}
-	type fields struct {
-		Address                 string
-		BindDN                  string
-		BindPass                string
-		UserBaseDn              string
-		UserBaseFilter          string
-		UserNameAttribute       string
-		GroupBaseDn             string
-		GroupNameAttribute      string
-		GroupNames              []string
-		StaticGroupSearchFilter string
-		StaticMemberAttribute   string
-		SearchRequestSizeLimit  int
-		SearchRequestTimeLimit  int
-		Ldaps                   bool
-		ForceBind               bool
-		tlsConfig               *tls.Config
-	}
-	type args struct {
-		conn *ldap.Conn
-		user string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    bool
-		wantErr bool
-	}{
-		{
-			name: "basic_test",
-			fields: fields{
-				Address:               "ldap.0xdata.loc:389",
-				BindDN:                "cn=admin,dc=0xdata,dc=loc",
-				BindPass:              "0xdata",
-				UserBaseDn:            "ou=users,dc=0xdata,dc=loc",
-				UserNameAttribute:     "uid",
-				GroupBaseDn:           "ou=groups,dc=0xdata,dc=loc",
-				GroupNameAttribute:    "cn",
-				StaticMemberAttribute: "memberUid",
-				GroupNames:            []string{"jettygroup", "steamgroup", "randomgroup"},
-				Ldaps:                 false,
-			},
-			args: args{
-				conn: conn,
-				user: "seb",
-			},
-			want:    true,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			l := &Ldap{
-				Address:                 tt.fields.Address,
-				BindDN:                  tt.fields.BindDN,
-				BindPass:                tt.fields.BindPass,
-				UserBaseDn:              tt.fields.UserBaseDn,
-				UserBaseFilter:          tt.fields.UserBaseFilter,
-				UserNameAttribute:       tt.fields.UserNameAttribute,
-				GroupBaseDn:             tt.fields.GroupBaseDn,
-				GroupNameAttribute:      tt.fields.GroupNameAttribute,
-				GroupNames:              tt.fields.GroupNames,
-				StaticGroupSearchFilter: tt.fields.StaticGroupSearchFilter,
-				StaticMemberAttribute:   tt.fields.StaticMemberAttribute,
-				SearchRequestSizeLimit:  tt.fields.SearchRequestSizeLimit,
-				SearchRequestTimeLimit:  tt.fields.SearchRequestTimeLimit,
-				Ldaps:     tt.fields.Ldaps,
-				ForceBind: tt.fields.ForceBind,
-				tlsConfig: tt.fields.tlsConfig,
-			}
-			got, err := l.checkGroup(tt.args.conn, tt.args.user)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Ldap.checkGroup() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("Ldap.checkGroup() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestLdap_TestConfig(t *testing.T) {
 	type fields struct {
 		Address                 string
@@ -197,6 +118,24 @@ func TestLdap_TestConfig(t *testing.T) {
 				"jettygroup": 8,
 			},
 			wantErr: false,
+		}, {
+			name: "AD_test",
+			fields: fields{
+				Address:               "ad.h2o.ai:389",
+				BindDN:                "CN=Test,CN=Users,DC=ad,DC=h2o,DC=ai",
+				BindPass:              adPass,
+				UserBaseDn:            "CN=Users,DC=ad,DC=h2o,DC=ai",
+				UserNameAttribute:     "sAMAccountName",
+				GroupBaseDn:           "OU=engineering,DC=ad,DC=h2o,DC=ai",
+				GroupNameAttribute:    "CN",
+				StaticMemberAttribute: "member",
+				GroupNames:            []string{"data science", "steamgroup"},
+			},
+			want: 2,
+			want1: map[string]int{
+				"data science": 2,
+			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
@@ -229,6 +168,122 @@ func TestLdap_TestConfig(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got1, tt.want1) {
 				t.Errorf("Ldap.TestConfig() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestLdap_checkGroup(t *testing.T) {
+	adConn, err := ldap.Dial("tcp", "ad.h2o.ai:389")
+	if err != nil {
+		t.Skipf("no AD connection found")
+	}
+	defer adConn.Close()
+	err = adConn.Bind("CN=Test,CN=Users,DC=ad,DC=h2o,DC=ai", adPass)
+	if err != nil {
+		t.Skipf("invalid bind")
+	}
+
+	conn, err := ldap.Dial("tcp", "ldap.0xdata.loc:389")
+	if err != nil {
+		t.Skipf("no ldap connection found")
+	}
+	defer conn.Close()
+
+	type fields struct {
+		Address                 string
+		BindDN                  string
+		BindPass                string
+		UserBaseDn              string
+		UserBaseFilter          string
+		UserNameAttribute       string
+		GroupBaseDn             string
+		GroupNameAttribute      string
+		GroupNames              []string
+		StaticGroupSearchFilter string
+		StaticMemberAttribute   string
+		SearchRequestSizeLimit  int
+		SearchRequestTimeLimit  int
+		Ldaps                   bool
+		ForceBind               bool
+		tlsConfig               *tls.Config
+	}
+	type args struct {
+		conn   *ldap.Conn
+		userCN string
+		userDN string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "basic_test",
+			fields: fields{
+				UserBaseDn:            "ou=users,dc=0xdata,dc=loc",
+				UserNameAttribute:     "uid",
+				GroupBaseDn:           "ou=groups,dc=0xdata,dc=loc",
+				GroupNameAttribute:    "cn",
+				StaticMemberAttribute: "memberUid",
+				GroupNames:            []string{"jettygroup", "steamgroup", "randomgroup"},
+			},
+			args: args{
+				conn:   conn,
+				userCN: "seb",
+				userDN: "cn=seb,ou=users,dc=0xdata,dc=loc",
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "AD_test",
+			fields: fields{
+				UserBaseDn:            "CN=Users,DC=ad,DC=h2o,DC=ai",
+				UserNameAttribute:     "sAMAccountName",
+				GroupBaseDn:           "OU=engineering,DC=ad,DC=h2o,DC=ai",
+				GroupNameAttribute:    "CN",
+				StaticMemberAttribute: "member",
+				GroupNames:            []string{"data science", "steamgroup"},
+			},
+			args: args{
+				conn:   adConn,
+				userCN: "datascientist",
+				userDN: "CN=datascientist,CN=Users,DC=ad,DC=h2o,DC=ai",
+			},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Ldap{
+				Address:                 tt.fields.Address,
+				BindDN:                  tt.fields.BindDN,
+				BindPass:                tt.fields.BindPass,
+				UserBaseDn:              tt.fields.UserBaseDn,
+				UserBaseFilter:          tt.fields.UserBaseFilter,
+				UserNameAttribute:       tt.fields.UserNameAttribute,
+				GroupBaseDn:             tt.fields.GroupBaseDn,
+				GroupNameAttribute:      tt.fields.GroupNameAttribute,
+				GroupNames:              tt.fields.GroupNames,
+				StaticGroupSearchFilter: tt.fields.StaticGroupSearchFilter,
+				StaticMemberAttribute:   tt.fields.StaticMemberAttribute,
+				SearchRequestSizeLimit:  tt.fields.SearchRequestSizeLimit,
+				SearchRequestTimeLimit:  tt.fields.SearchRequestTimeLimit,
+				Ldaps:     tt.fields.Ldaps,
+				ForceBind: tt.fields.ForceBind,
+				tlsConfig: tt.fields.tlsConfig,
+			}
+			got, err := l.checkGroup(tt.args.conn, tt.args.userCN, tt.args.userDN)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Ldap.checkGroup() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Ldap.checkGroup() = %v, want %v", got, tt.want)
 			}
 		})
 	}
