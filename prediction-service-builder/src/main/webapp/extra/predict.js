d7a1d3d2d912f969b8ecfeab6cb51a9574f438be
Justin Loyola <justin@h2o.ai>
2017-05-10 19:06:37 -0700

    Fix property names

--- prediction-service-builder/src/main/webapp/extra/predict.js
+++ prediction-service-builder/src/main/webapp/extra/predict.js
@@ -55,8 +55,8 @@
 
 // Form for model, shown in element
   function showModel(model, element) {
-    var names = model.m_names;
-    var domains = model.m_domains;
+    var names = model.m._names;
+    var domains = model.m._domains;
     var domainMap = model.domainMap;
 
     var form = '<legend>Parameters</legend>';
@@ -153,7 +153,7 @@
   function showInputParameters() {
     $.get(API_HOST + '/info', function(data, status) {
       // show result
-      if (data.problem_type === 'image') {
+      if (data.m.problem_type === 'image') {
         isBinaryPrediction = true;
         hideBatch();
       } else {
